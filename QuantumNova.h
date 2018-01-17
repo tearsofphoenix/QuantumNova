5d81cd5e6c6ee5079ce84a4afc3d4c5df9f40dc0
tearsofphoenix <tearsofphoenix@icloud.com>
2018-01-17 11:26:15 +0800

    remove useless files

--- QuantumNova.h
+++ QuantumNova.h
@@ -1,5 +1,8 @@
 
+#include "src/QCClass.h"
+#include "src/QCObject.h"
 #include "src/QCArray.h"
 #include "src/QCKey.h"
+#include "src/QCMessage.h"
 #include "src/QCRandom.h"
 #include "src/QCCipher.h"
